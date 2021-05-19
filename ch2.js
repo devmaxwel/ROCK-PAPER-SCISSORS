1aff969ee6738962cbe4b0241bce4f52eb748989
MAXWEL OCHIENG <64557216+Maxwel-Ochieng@users.noreply.github.com>
2021-05-19 15:47:05 +0300

    Add files via upload

---
+++ ch2.js
@@ -0,0 +1,8 @@
+//Challenge 2
+function GenCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('cat-gen-butn');
+    image.style.width ='20%'
+    image.src=  "C:/Users/MAXWEL/Desktop/JAVASCRIPT CRASH COURSE/Challenge2/rock.jpg"
+    div.appendChild(image); 
+}
